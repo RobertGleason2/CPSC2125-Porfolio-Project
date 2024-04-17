6a001b62e7c72af33c650383cc9c55ec0d6e4119
Robert Gleason <91793071+RobertGleason2@users.noreply.github.com>
2024-04-17 01:09:52 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//declares the function 
+function changeBackgroundColorBlue() {
+  //changes the background of the grid cell based off of the ID (specified to be Article) to the color blue
+   document.getElementById("Article").style.backgroundColor = "#4556d9";
+}
+//declares the function
+function changeBackgroundColorRed() {
+  //changes the background of the grid cell based off of the ID (specified to be Article) to the color red
+   document.getElementById("Article").style.backgroundColor = "#c82d2d";
+}
+//declares the function
+function changeBackgroundColorGreen() {
+  //changes the background of the grid cell based off of the ID (specified to be Article) to the color lightgreen
+   document.getElementById("Article").style.backgroundColor = "lightgreen";
+}
